f34cd8dee7f06da2085f802b23d3c129f802d550
nsegreto <nsegreto@gmail.com>
2012-12-03 22:29:45 -0700

    TS120312

---
+++ Opentok-PHP/gensessionid_token.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once './Opentok-PHP-SDK/API_Config.php';
+require_once './Opentok-PHP-SDK/OpenTokSDK.php';
+
+// Creating an OpenTok Object
+$apiObj = new OpenTokSDK( API_Config::API_KEY, API_Config::API_SECRET );
+
+// Creating Simple Session object, passing IP address to determine closest production server
+// Passing IP address to determine closest production server
+$session = $apiObj->createSession( $_SERVER["REMOTE_ADDR"] );
+
+// Creating Simple Session object 
+// Enable p2p connections
+//$session = $apiObj->createSession( $_SERVER["REMOTE_ADDR"], array(SessionPropertyConstants::P2P_PREFERENCE=> "enabled") );
+
+// Getting sessionId from Sessions
+// Option 1: Call getSessionId()
+$sessionId = $session->getSessionId();
+
+// After creating a session, call generateToken(). Require parameter: SessionId
+$token = $apiObj->generateToken($sessionId);
+
+// Giving the token a moderator role, expire time 5 days from now, and connectionData to pass to other users in the session
+//$token = $apiObj->generateToken($sessionId, RoleConstants::MODERATOR, time() + (5*24*60*60), "hello world!" );
+
+$arr =  array();
+$arr['token'] = $token;
+$arr['sessionId'] = $sessionId;
+$arr['apiKey'] = '1127';
+	
+
+echo $_GET['callback']. '('. json_encode($arr) . ')';  
+
+?>
--- TBkeys.js
+++ TBkeys.js
@@ -1,15 +1,33 @@
+var sessionUrl = "http://app.itelepsych.com/Opentok-PHP/gensessionid_token.php";
+
+document.write("<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>");
+
+
 function getTBsessionId(){
-	var id = "1_MX4xMTI3fn5UaHUgTm92IDI5IDE1OjAzOjM0IFBTVCAyMDEyfjAuOTM2MjgwOTd-";
-	return id;
+	return TBSessionParams.sessionId;
 	}
 
 function getTBtoken(){
-	var token = "T1==cGFydG5lcl9pZD0xMTI3JnNpZz0yNjQxNDk2YTAyZmQ3YzVlZjViNzQxZTFkYTQ2ZTI1ZGUwNjgyMGY5OnNlc3Npb25faWQ9MV9NWDR4TVRJM2ZuNVVhSFVnVG05MklESTVJREUxT2pBek9qTTBJRkJUVkNBeU1ERXlmakF1T1RNMk1qZ3dPVGQtJmNyZWF0ZV90aW1lPTEzNTQyMzAyMTMmZXhwaXJlX3RpbWU9MTM1NDMxNjYxMyZyb2xlPXB1Ymxpc2hlciZub25jZT04NDI4NjE=";
-	return token;
+	return TBSessionParams.token;
 }
 
 function getTBapiKey(){
 	var apiKey = '1127';
-	console.log(apiKey);
-	return apiKey;
+	return TBSessionParams.apiKey;
+}
+
+var TBSessionParams;
+
+function session_token(){
+	var theUrl = sessionUrl;
+	$.ajax({
+        async: false,
+        type: 'GET',
+        url: theUrl,
+        dataType: 'jsonp',
+        success: function (json) {
+        	TBSessionParams = json;
+        }
+    });
+	
 }
