30d61efde3c4f2cd192a20322fd7e00db66298d2
nsegreto <nsegreto@gmail.com>
2012-12-05 07:55:59 -0700

    TS120512

--- TBkeys2.js
+++ TBkeys2.js
@@ -1,10 +1,19 @@
+document.write("<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>");
+document.write("<script type='text/javascript' src='http://static.opentok.com/webrtc/v2.0/js/TB.min.js'></script>'");
+
+var TBSessionParams;
+var readyTB = false;
+
+function session_token(){
+}
+
 function getTBsessionId(){
-	var id = "1_MX4xMTQyMTg3Mn4xMjcuMC4wLjF-VHVlIERlYyAwNCAxMDo0Nzo1OCBQU1QgMjAxMn4wLjI4MTk3NTkyfg";
+	var id = "1_MX4xMTQyMTg3Mn4xMjcuMC4wLjF-VHVlIERlYyAwNCAxODozNzowMiBQU1QgMjAxMn4wLjIxODMzNzQ4fg";
 	return id;
 	}
 
 function getTBtoken(){
-	var token = "T1==cGFydG5lcl9pZD0xMTQyMTg3MiZzaWc9NDdhYjZkNzVkOWQ0ZjQ2ZDExNjIyNjAwMGYxMWQ2Y2VjMTg0Y2MxNDpzZXNzaW9uX2lkPTFfTVg0eE1UUXlNVGczTW40eE1qY3VNQzR3TGpGLVZIVmxJRVJsWXlBd05DQXhNRG8wTnpvMU9DQlFVMVFnTWpBeE1uNHdMakk0TVRrM05Ua3lmZyZjcmVhdGVfdGltZT0xMzU0NjQ2ODgyJnJvbGU9cHVibGlzaGVyJm5vbmNlPTEzNTQ2NDY4ODIuODQ4NTE5ODI5MDg1ODQ=";
+	var token = "T1==cGFydG5lcl9pZD0xMTQyMTg3MiZzaWc9ZmNkOWZiNmU5OGM0ZGU5M2ZjMjQ3MzQ2ZDliMjM0ZTdlYzJkZWYzNjpzZXNzaW9uX2lkPTFfTVg0eE1UUXlNVGczTW40eE1qY3VNQzR3TGpGLVZIVmxJRVJsWXlBd05DQXhPRG96Tnpvd01pQlFVMVFnTWpBeE1uNHdMakl4T0RNek56UTRmZyZjcmVhdGVfdGltZT0xMzU0Njc1MDE3JnJvbGU9cHVibGlzaGVyJm5vbmNlPTEzNTQ2NzUwMTcuNzUxMzQ1MDYxMjEx";
 	return token;
 }
 
@@ -13,4 +22,18 @@ function getTBapiKey(){
 	console.log(apiKey);
 	return apiKey;
 }
+
+function TBisready(){
+	if(typeof readyTB == 'undefined'){
+		console.log('readyTB is undefined');
+		return false;
+	}
+	else {
+		if( !readyTB )
+			return false;
+		else
+			return true;
+	}
+}
+
 
--- js/session.js
+++ js/session.js
@@ -21,12 +21,14 @@ var apiKey;
 var sessionId;
 var token;
 var session;
+var	publisher;
 
-function doVideo() {
+function publishStart() {
 	TB.addEventListener("exception", exceptionHandler);
 	session = TB.initSession(getTBsessionId()); // Replace with your own session ID. See https://dashboard.tokbox.com/projects
 	session.addEventListener("sessionConnected", sessionConnectedHandler);
 	session.addEventListener("streamCreated", streamCreatedHandler);
+	session.addEventListener('sessionDisconnected', sessionDisconnectedHandler);
 	session.connect(getTBapiKey(), getTBtoken()); 		// Replace with your API key and token. See https://dashboard.tokbox.com/projects
 	                                        // and https://dashboard.tokbox.com/projects
 
@@ -34,7 +36,10 @@ function doVideo() {
 
 function sessionConnectedHandler(event) {
     var publisherContainer = document.getElementById("pos1");
-    var publisherProperties = {width: 200, height:200, name:"Bob's stream"};
+    var publisherProperties = {width: 200, height:200, name:getUseragent()};
+    alert("PublisherContainer: "+ publisherContainer);
+    alert("publisherProperties: "+ publisherProperties);
+    
     publisher = TB.initPublisher(getTBapiKey(), publisherContainer.id, publisherProperties);
 		 
 		 session.publish(publisher);
@@ -43,13 +48,25 @@ function sessionConnectedHandler(event) {
 	
 	function streamCreatedHandler(event) {
 		subscribeToStreams(event.streams);
+		var subscriberProps = {	width: 200, height: 200, subscribeToAudio: true};
+		var position = document.getElementById('pos2');
+
+		//event.target.subscribe(event.streams[0], position.id, subscriberProps);
+		streams = event.streams;
+
 	}
 	
 	function subscribeToStreams(streams) {
+		console.log("------------------------------------------------------------------------");
+		console.log("subscribe to streams");
+		console.log("------------------------------------------------------------------------");
+		var subscriberProps = {	width: 200, height: 200, subscribeToAudio: true};
+		var position = document.getElementById('pos2');
+		
 		for (i = 0; i < streams.length; i++) {
 			var stream = streams[i];
 			if (stream.connection.connectionId != session.connection.connectionId) {
-				session.subscribe(stream);
+				session.subscribe(stream, position.id, subscriberProps);
 			}
 		}
 	}
@@ -72,3 +89,31 @@ function sessionConnectedHandler(event) {
 		var a = document.getElementById("sessionId");
 		a.innerHTML = getTBsessionId();
 	}			
+
+	function getUseragent(){
+		if(navigator.userAgent.match(/Linux/i)){
+			return 'Linux';
+		}
+		else if (navigator.userAgent.match(/Macintosh/i))
+			return 'Macintosh';
+		else
+			return 'unknown';
+	}
+	
+	
+	function publishEnd(){
+		
+		//session.unpublish(publisher);
+		session.disconnect();
+		
+	}
+	
+	
+	function sessionDisconnectedHandler(event) {
+		// This signals that the user was disconnected from the Session. Any subscribers and publishers
+		// will automatically be removed. This default behaviour can be prevented using event.preventDefault()
+		publisher = null;
+
+;
+	}
+	
